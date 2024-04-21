14018c3aa6f4f202ef67c56a2498bac418600c59
TylerFix <107802149+TylerFix@users.noreply.github.com>
2024-04-21 21:51:31 +0300

    Добавлены 2 перы адресов по Харькову, одновление графика через чек бокс, выбор даты выбора данных, переписана БД под новые адреса, переделан костыль расчета процентов и записи в БД, реализован через счетчик.

--- ltt.py
+++ ltt.py
@@ -5,12 +5,14 @@ import sqlite3
 import datetime
 
 #  Адресса для просчета
-streets_list_otkuda = ['Тракторобудівників проспект, 142/1','Ст. Метро "Держпром"','ТЦ "Дафі" (вхід ближче до Roshen)']
-streets_list_kuda = ['Салтівське шосе, 262А/5','ТРЦ "Нікольський"','Ювілейний (50-річчя ВЛКСМ) проспект, 34/1']
-value_list = [None,None,None]
-value_prec = [None,None,None]
+streets_list_otkuda = ['Тракторобудівників проспект, 142/1','Ст. Метро "Держпром"','ТЦ "Дафі" (вхід ближче до Roshen)','Георгія Тарасенка (Плеханівська) вулиця, 43/1','Плиткова вулиця, 4']
+streets_list_kuda = ['Салтівське шосе, 262А/5','ТРЦ "Нікольський"','Ювілейний (50-річчя ВЛКСМ) проспект, 34/1','Морозова вулиця, 20А/1','Луї Пастера вулиця, 234А/1']
+count_streets = len(streets_list_otkuda)-1
+print(count_streets)
+value_list = [None,None,None,None,None]
+value_prec = [None,None,None,None,None]
 count_et = 0
-basic_price = [80,54,77]
+basic_price = [80,54,77,84,56]
 # DB
 conn = sqlite3.connect('prices.db')
 cursor = conn.cursor()
@@ -20,6 +22,8 @@ id INTEGER PRIMARY KEY,
 trakt_salt INTEGER,
 derzhprom_nikolskii INTEGER,
 dafi_uvileynii INTEGER,
+plechan_moroz INTEGER,
+plitk_pastera INTEGER,
 time TEXT)
 ''')
 cursor.execute('''
@@ -28,24 +32,26 @@ id INTEGER PRIMARY KEY,
 trakt_salt INTEGER,
 derzhprom_nikolskii INTEGER,
 dafi_uvileynii INTEGER,
+plechan_moroz INTEGER,
+plitk_pastera INTEGER,
 time TEXT)
 ''')
 browser = webdriver.Chrome('')
 browser.get('https://ontaxi.com.ua/ru/kharkiv')
-def calculation_price (streets_list_otkuda,streets_list_kuda,value_list,value_prec,count_et,basic_price):
+def calculation_price (streets_list_otkuda,streets_list_kuda,value_list,value_prec,count_et,basic_price,count_streets):
     while True:
         try:
             # Тело обновление сравнений
             element_otkuda = browser.find_element('xpath','//*[@id="inputplace0"]')
             element_otkuda.send_keys(streets_list_otkuda[count_et]) 
-            sleep(2)
+            sleep(3)
             element_otkuda.send_keys(Keys.ENTER) # костыль
-            sleep(2)
+            sleep(1)
             element_kuda = browser.find_element('xpath', '//*[@id="inputplace1"]')
             element_kuda.send_keys(streets_list_kuda[count_et])
-            sleep(2)
+            sleep(3)
             element_kuda.send_keys(Keys.ENTER)
-            sleep(2)
+            sleep(1)
             element_money = browser.find_element('xpath','//*[@id="orderForm"]/div[2]/div/div[1]/div[5]/div[2]/span/span[1]/span')
             print('Значение : ',element_money.text)
             value_list[count_et] = element_money.text
@@ -56,9 +62,9 @@ def calculation_price (streets_list_otkuda,streets_list_kuda,value_list,value_pr
                 element_otkuda.send_keys(Keys.BACKSPACE)
             for i in range (0,l_adress_kuda):
                 element_kuda.send_keys(Keys.BACKSPACE)
-            # Конец тела обновления
+            # Конец тела сравнений
             # Калькулятор
-            if count_et == 0 : # костыль
+            if count_et != count_streets : # костыль
                 diff_price = int(value_list[count_et]) - basic_price[count_et]
                 if diff_price != 0:
                     perc_value = int(value_list[count_et])*0.01
@@ -68,17 +74,7 @@ def calculation_price (streets_list_otkuda,streets_list_kuda,value_list,value_pr
                     value_prec[count_et] = 0
                 count_et +=1
             
-            elif count_et == 1 :
-                diff_price = int(value_list[count_et]) - basic_price[count_et]
-                if diff_price != 0:
-                    perc_value = int(value_list[count_et])*0.01
-                    value_prec[count_et] = round(diff_price/perc_value)
-                    print('Вторая пара ',value_prec[count_et])
-                else: 
-                    value_prec[count_et] = 0
-                count_et +=1
-            
-            elif count_et == 2:
+            elif count_et == count_streets:
                 diff_price = int(value_list[count_et]) - basic_price[count_et]
                 if diff_price != 0:
                     perc_value = int(value_list[count_et])*0.01
@@ -90,9 +86,9 @@ def calculation_price (streets_list_otkuda,streets_list_kuda,value_list,value_pr
                 formatted_time = now.strftime('%Y-%m-%d %H:%M:%S')
                 print(formatted_time)
                 # Запись в БД
-                cursor.execute('INSERT INTO Price (trakt_salt, derzhprom_nikolskii, dafi_uvileynii, time) VALUES (?, ?, ?, ?)', (int(value_list[0]), int(value_list[1]),int(value_list[2]),str(formatted_time)))
+                cursor.execute('INSERT INTO Price (trakt_salt, derzhprom_nikolskii, dafi_uvileynii, plechan_moroz, plitk_pastera, time) VALUES (?, ?, ?, ?, ?, ?)', (int(value_list[0]), int(value_list[1]),int(value_list[2]),int(value_list[3]),int(value_list[4]),str(formatted_time)))
                 conn.commit()
-                cursor.execute('INSERT INTO Percent (trakt_salt, derzhprom_nikolskii, dafi_uvileynii, time) VALUES (?, ?, ?, ?)', (int(value_prec[0]), int(value_prec[1]),int(value_prec[2]),str(formatted_time)))
+                cursor.execute('INSERT INTO Percent (trakt_salt, derzhprom_nikolskii, dafi_uvileynii,plechan_moroz, plitk_pastera, time) VALUES (?, ?, ?, ?, ?, ?)', (int(value_prec[0]), int(value_prec[1]),int(value_prec[2]),int(value_prec[3]),int(value_prec[4]),str(formatted_time)))
                 conn.commit()
                 count_et = 0
             
@@ -100,4 +96,4 @@ def calculation_price (streets_list_otkuda,streets_list_kuda,value_list,value_pr
             print('Ошибка : ', e)
         
 
-calculation_price(streets_list_otkuda,streets_list_kuda,value_list,value_prec,count_et,basic_price)
+calculation_price(streets_list_otkuda,streets_list_kuda,value_list,value_prec,count_et,basic_price,count_streets)
--- start_graf_dash.py
+++ start_graf_dash.py
@@ -9,29 +9,33 @@ import sqlite3
 app = dash.Dash(__name__)
 
 # Функция для получения данных из базы данных и обновления графика
-def update_data_and_graph(interval):
-    # Подключение к базе данных SQLite
+def update_data_and_graph(start_time, end_time):
+
     conn = sqlite3.connect('prices.db')
     cursor = conn.cursor()
 
     try:
-        # Запрос данных из таблицы Price
-        cursor.execute('SELECT * FROM Price')
+        # Запрос данных из таблицы Price за указанный временной интервал
+        cursor.execute('SELECT * FROM Price WHERE time BETWEEN ? AND ?', (start_time, end_time))
         price_data = cursor.fetchall()
 
         # Разделение данных на списки для каждого столбца
-        time_price = [row[4] for row in price_data]
+        time_price = [row[6] for row in price_data]
         trakt_salt_price = [int(row[1]) for row in price_data]
         derzhprom_nikolskii_price = [int(row[2]) for row in price_data]
         dafi_uvileynii_price = [int(row[3]) for row in price_data]
+        plechan_moroz_price = [row[4] for row in price_data]
+        plitk_pastera_price = [row[5] for row in price_data]
+        #plechan_moroz, plitk_pastera,80,54,77
 
         # Создание графика
         fig_price = go.Figure()
-        fig_price.add_trace(go.Scatter(x=time_price, y=trakt_salt_price, mode='lines', name='trakt_salt'))
-        fig_price.add_trace(go.Scatter(x=time_price, y=derzhprom_nikolskii_price, mode='lines', name='derzhprom_nikolskii'))
-        fig_price.add_trace(go.Scatter(x=time_price, y=dafi_uvileynii_price, mode='lines', name='dafi_uvileynii'))
+        fig_price.add_trace(go.Scatter(x=time_price, y=trakt_salt_price, mode='lines', name='Тракторобудівників проспект, 142/1 - Салтівське шосе, 262А/5, 80 грн'))
+        fig_price.add_trace(go.Scatter(x=time_price, y=derzhprom_nikolskii_price, mode='lines', name='Ст. Метро "Держпром" - ТРЦ "Нікольський", 54 грн'))
+        fig_price.add_trace(go.Scatter(x=time_price, y=dafi_uvileynii_price, mode='lines', name='ТЦ "Дафі" (вхід ближче до Roshen) - Ювілейний (50-річчя ВЛКСМ) проспект, 34/1, 77 грн'))
+        fig_price.add_trace(go.Scatter(x=time_price, y=plechan_moroz_price, mode='lines', name='Георгія Тарасенка (Плеханівська) вулиця, 43/1 - Морозова вулиця, 20А/1, 84 грн'))
+        fig_price.add_trace(go.Scatter(x=time_price, y=plitk_pastera_price, mode='lines', name='Плиткова вулиця, 4 - Луї Пастера вулиця, 234А/1, 56 грн'))
         fig_price.update_layout(title='Цены OnTaxi Харьков', xaxis_title='Время', yaxis_title='Цена')
-        
 
         return fig_price
     except Exception as e:
@@ -44,14 +48,31 @@ def update_data_and_graph(interval):
 # Определение макета веб-приложения
 app.layout = html.Div(children=[
     dcc.Graph(id='price-graph'),
+    dcc.DatePickerRange(
+        id='date-picker-range',
+        start_date=datetime.datetime.now() - datetime.timedelta(days=1),
+        end_date=datetime.datetime.now()
+    ),
+    html.Label('Автоматическое обновление данных:'),
+    dcc.Checklist(
+        id='auto-update-checklist',
+        options=[{'label': 'Включено', 'value': 'enabled'}],
+        value=[]
+    ),
     dcc.Interval(id='interval-component', interval=10*1000, n_intervals=0)
 ])
 
-# Определение функции обновления графика по интервалу
+# Определение функции обновления графика по интервалу и при включении чек-бокса
 @app.callback(Output('price-graph', 'figure'),
-              [Input('interval-component', 'n_intervals')])
-def update_graph(n):
-    return update_data_and_graph(n)
+              [Input('date-picker-range', 'start_date'),
+               Input('date-picker-range', 'end_date'),
+               Input('auto-update-checklist', 'value'),
+               Input('interval-component', 'n_intervals')])
+def update_graph(start_date, end_date, auto_update_value, n_intervals):
+    if 'enabled' in auto_update_value:
+        return update_data_and_graph(start_date, end_date)
+    else:
+        return dash.no_update
 
 # Запуск приложения Dash
 if __name__ == '__main__':
