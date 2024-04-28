5775409165f46d1248d353a1ca60cd9cfab405f2
TylerFix <9naval99@gmail.com>
2024-04-28 22:54:43 +0300

    Добавлено 4 города, к ним графики, изменена БД, изменен алгоритм адресов, добавлена матрица и потоки

--- ltt.py
+++ ltt.py
@@ -4,27 +4,52 @@ from time import sleep
 import sqlite3
 import datetime
 from selenium.webdriver.chrome.options import Options
+import threading
+import subprocess
+import webbrowser
 
 #  Адресса для просчета
-streets_list_otkuda = ['Тракторобудівників проспект, 142/1','Ст. Метро "Держпром"','ТЦ "Дафі" (вхід ближче до Roshen)','Георгія Тарасенка (Плеханівська) вулиця, 43/1','Плиткова вулиця, 4']
-streets_list_kuda = ['Салтівське шосе, 262А/5','ТРЦ "Нікольський"','Ювілейний (50-річчя ВЛКСМ) проспект, 34/1','Морозова вулиця, 20А/1','Луї Пастера вулиця, 234А/1']
+streets_list_otkuda = [['Тракторобудівників проспект, 142/1','Ст. Метро "Держпром"','ТЦ "Дафі" (вхід ближче до Roshen)','Георгія Тарасенка (Плеханівська) вулиця, 43/1','Плиткова вулиця, 4'],
+                       ['Героїв Крут (12 Квітня) вулиця, 3','Авраменка вулиця, 1','Балка Поповка вулиця, 21','Рубана вулиця, 20','Тенісна вулиця, 20'],
+                       ['Європейська (Миронова) вулиця, 8А','Запорізьке шосе, 72','Набережна Перемоги вулиця, 134','Переволочанський (Новочеркаський) провулок, 5','Зоряна вулиця, 4'],
+                       ['Французька (Комінтернівська) вулиця, 7','Індустріальний мікрорайон, 68','Проскурівська вулиця, 31А','Турнірна вулиця, 10','Старотернівська (Глазунова) вулиця, 106'],
+                       ['І. Мазепи (Мануїльського) вулиця, 164','Новогоголівська вулиця, 42/1','Корольова вулиця, 101','Покровська (Щорса) вулиця, 142','Князькова вулиця, 39А']]
+streets_list_kuda = [['Салтівське шосе, 262А/5','ТРЦ "Нікольський"','Ювілейний (50-річчя ВЛКСМ) проспект, 34/1','Морозова вулиця, 20А/1','Луї Пастера вулиця, 234А/1'],
+                     ['Перемоги вулиця, 87В','Аптечна вулиця, 3','Дальня вулиця, 41/2','Будівельників бульвар, 1','Теплична вулиця, 1'],
+                     ['Макарова вулиця, 1А','Запорізьке шосе, 27Б','Набережна Перемоги вулиця, 2Г','Слобожанський (Газети "Правда") проспект, 5А','Славни вулиця, 1'],
+                     ['Автомеханічна (Кисловодська) вулиця, 1','Сонячний мікрорайон, 3','Говерлівська (Самойлова) вулиця, 82','Груднева вулиця, 1','Центральна вулиця, 1А'],
+                     ['Пункт Незламності (Епіцентр)','Хінчанський проїзд, 1/67','Комунальний провулок, 1','Пункт Незламності (Нова пошта №27)','Є. Коновальця (Якубовського) вулиця, 7А']]
+web_list = ['https://ontaxi.com.ua/ru/kharkiv','https://ontaxi.com.ua/ru/zaporizhzhya','https://ontaxi.com.ua/ru/dnipro','https://ontaxi.com.ua/ru/kryvyi-rih','https://ontaxi.com.ua/ru/zhytomyr']
 count_streets = len(streets_list_otkuda)-1
-print(count_streets)
-value_list = [None,None,None,None,None]
-value_prec = [None,None,None,None,None]
+cities_count = len(web_list)-1
+print(f'Улиц для расчета - {count_streets}')
+print(f'Городов для расчета - {cities_count}')
+#value_list = [None,None,None,None,None] # HARKIV
+value_prec = [None,None,None,None,None] # HARKIV
 count_et = 0
-basic_price = [80,54,77,84,56]
+basic_price = [80,54,77,84,56] # HARKIV
+db_save_list = ['PriceHark','PriceZap','PriceDnepr','PriceKR','PriceZhitom']
+db_conn = [None,None,None,None,None]
+db_cur = [None,None,None,None,None]
+url = 'http://127.0.0.1:8050/'
+
+
 # DB
-conn = sqlite3.connect('prices.db')
+conn = sqlite3.connect('prices.db',check_same_thread=False)
 cursor = conn.cursor()
 cursor.execute('''
-CREATE TABLE IF NOT EXISTS Price(
+CREATE TABLE IF NOT EXISTS PriceHark(
 id INTEGER PRIMARY KEY,
 trakt_salt INTEGER,
 derzhprom_nikolskii INTEGER,
 dafi_uvileynii INTEGER,
 plechan_moroz INTEGER,
 plitk_pastera INTEGER,
+dif_trakt_salt INTEGER,
+dif_derzhprom_nikolskii INTEGER,
+dif_dafi_uvileynii INTEGER,
+dif_plechan_moroz INTEGER,
+dif_plitk_pastera INTEGER,
 time TEXT)
 ''')
 cursor.execute('''
@@ -37,31 +62,98 @@ plechan_moroz INTEGER,
 plitk_pastera INTEGER,
 time TEXT)
 ''')
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS PriceZap(
+id INTEGER PRIMARY KEY,
+krut_perem INTEGER,
+avram_aptech INTEGER,
+popovka_daln INTEGER,
+ruban_budiv INTEGER,
+tenis_tepli INTEGER,
+dif_krut_perem INTEGER,
+dif_avram_aptech INTEGER,
+dif_popovka_daln INTEGER,
+dif_ruban_budiv INTEGER,
+dif_tenis_tepli INTEGER,
+time TEXT)
+''')
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS PriceDnepr(
+id INTEGER PRIMARY KEY,
+mironova_makar INTEGER,
+zapor_zapor INTEGER,
+peremog_peremog INTEGER,
+novoch_pravdi INTEGER,
+zoryana_zlavni INTEGER,
+dif_mironova_makar INTEGER,
+dif_zapor_zapor INTEGER,
+dif_peremog_peremog INTEGER,
+dif_novoch_pravdi INTEGER,
+dif_zoryana_zlavni INTEGER,
+time TEXT)
+''')
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS PriceKR(
+id INTEGER PRIMARY KEY,
+franc_avto INTEGER,
+indus_sonyach INTEGER,
+prosk_goverl INTEGER,
+turn_grud INTEGER,
+glazunov_centr INTEGER,
+dif_franc_avto INTEGER,
+dif_indus_sonyach INTEGER,
+dif_prosk_goverl INTEGER,
+dif_turn_grud INTEGER,
+dif_glazunov_centr INTEGER,
+time TEXT)
+''')
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS PriceZhitom(
+id INTEGER PRIMARY KEY,
+mazep_epic INTEGER,
+novog_hinch INTEGER,
+korol_komun INTEGER,
+shors_novaposh INTEGER,
+knyas_koval INTEGER,
+dif_mazep_epic INTEGER,
+dif_novog_hinch INTEGER,
+dif_korol_komun INTEGER,
+dif_chors_novaposh INTEGER,
+dif_knyas_koval INTEGER,
+time TEXT)
+''')
+# Конец DB
+
 
 # Создание объекта опций для Chrome
 chrome_options = Options()
-
-# Установка опции headless в True
-chrome_options.headless = True
-browser = webdriver.Chrome(options=chrome_options)
-browser.get('https://ontaxi.com.ua/ru/kharkiv')
-def calculation_price (streets_list_otkuda,streets_list_kuda,value_list,value_prec,count_et,basic_price,count_streets):
+# Функция парсинга
+def calculation_price (streets_list_otkuda,streets_list_kuda,value_prec,count_et,basic_price,count_streets,web_list,db_count,db_conn,db_cur):
+    chrome_options.headless = True
+    #browser = webdriver.Chrome(options=chrome_options)
+    print('Счетчик базы данных',db_count)
+    db_conn[db_count] = sqlite3.connect('prices.db', check_same_thread=False)
+    db_cur[db_count] = db_conn[db_count].cursor()
+    browser = webdriver.Chrome(options=chrome_options)
+    browser.get(web_list[db_count])
+    value_list = [None,None,None,None,None]
     while True:
         try:
             # Тело обновление сравнений
             element_otkuda = browser.find_element('xpath','//*[@id="inputplace0"]')
-            element_otkuda.send_keys(streets_list_otkuda[count_et]) 
+            element_otkuda.send_keys(streets_list_otkuda[db_count][count_et]) 
             sleep(2)
-            element_otkuda.send_keys(Keys.ENTER) # костыль
+            element_otkuda.send_keys(Keys.ENTER) 
             sleep(1)
             element_kuda = browser.find_element('xpath', '//*[@id="inputplace1"]')
-            element_kuda.send_keys(streets_list_kuda[count_et])
+            element_kuda.send_keys(streets_list_kuda[db_count][count_et])
             sleep(2)
             element_kuda.send_keys(Keys.ENTER)
             sleep(1)
             element_money = browser.find_element('xpath','//*[@id="orderForm"]/div[2]/div/div[1]/div[5]/div[2]/span/span[1]/span')
             print('Значение : ',element_money.text)
-            value_list[count_et] = element_money.text
+            if element_money.text != 50:
+                value_list[count_et] = element_money.text
             l_adress_otkuda = len(streets_list_otkuda[count_et])
             l_adress_kuda = len(streets_list_kuda[count_et])
 
@@ -71,12 +163,12 @@ def calculation_price (streets_list_otkuda,streets_list_kuda,value_list,value_pr
                 element_kuda.send_keys(Keys.BACKSPACE)
             # Конец тела сравнений
             # Калькулятор
-            if count_et != count_streets : # костыль
+            if count_et != count_streets : #
                 diff_price = int(value_list[count_et]) - basic_price[count_et]
                 if diff_price != 0:
                     perc_value = int(value_list[count_et])*0.01
                     value_prec[count_et] = round(diff_price/perc_value)
-                    print('Первая пара ',value_prec[count_et])
+                    print(f'{count_et+1} пара ',value_prec[count_et])
                 else: 
                     value_prec[count_et] = 0
                 count_et +=1
@@ -86,21 +178,33 @@ def calculation_price (streets_list_otkuda,streets_list_kuda,value_list,value_pr
                 if diff_price != 0:
                     perc_value = int(value_list[count_et])*0.01
                     value_prec[count_et] = round(diff_price/perc_value)
-                    print('Третья пара ',value_prec[count_et])
+                    print(f'{count_et+1} пара ',value_prec[count_et])
                 else: 
                     value_prec[count_et] = 0
                 now = datetime.datetime.now()
                 formatted_time = now.strftime('%Y-%m-%d %H:%M:%S')
                 print(formatted_time)
                 # Запись в БД
-                cursor.execute('INSERT INTO Price (trakt_salt, derzhprom_nikolskii, dafi_uvileynii, plechan_moroz, plitk_pastera, time) VALUES (?, ?, ?, ?, ?, ?)', (int(value_list[0]), int(value_list[1]),int(value_list[2]),int(value_list[3]),int(value_list[4]),str(formatted_time)))
-                conn.commit()
-                cursor.execute('INSERT INTO Percent (trakt_salt, derzhprom_nikolskii, dafi_uvileynii,plechan_moroz, plitk_pastera, time) VALUES (?, ?, ?, ?, ?, ?)', (int(value_prec[0]), int(value_prec[1]),int(value_prec[2]),int(value_prec[3]),int(value_prec[4]),str(formatted_time)))
-                conn.commit()
+                db_cur[db_count].execute(f'INSERT INTO {db_save_list[db_count]} VALUES (NULL, ?, ?, ?, ?, ?, 0, 0, 0, 0 , 0, ?)', (int(value_list[0]), int(value_list[1]),int(value_list[2]),int(value_list[3]),int(value_list[4]),str(formatted_time)))
+                db_conn[db_count].commit()
+                #cursor.execute('INSERT INTO Percent (trakt_salt, derzhprom_nikolskii, dafi_uvileynii,plechan_moroz, plitk_pastera, time) VALUES (?, ?, ?, ?, ?, ?)', (int(value_prec[0]), int(value_prec[1]),int(value_prec[2]),int(value_prec[3]),int(value_prec[4]),str(formatted_time)))
+                #conn.commit()
                 count_et = 0
             
         except Exception as e:
             print('Ошибка : ', e)
-        
-
-calculation_price(streets_list_otkuda,streets_list_kuda,value_list,value_prec,count_et,basic_price,count_streets)
+# Старт новых потоков        
+threads =[]
+for i in range(cities_count+1):
+    print(f'Открываю потоки попытка {i}')
+    try:
+        thread = threading.Thread(target=calculation_price, args=(streets_list_otkuda,streets_list_kuda,value_prec,count_et,basic_price,count_streets,web_list,i,db_conn,db_cur))
+        thread.start()
+        threads.append(thread)
+        print (f'Поток {i+1} запущен с городом {web_list[i]}')
+        sleep (5)
+    except Exception as c:
+        print('Не запустил поток. Ошибка : ', c)
+webbrowser.open(url)
+print('Открыл адресс - ', url)
+subprocess.run(['python','start_graf_dash.py'])
--- start_graf_dash.py
+++ start_graf_dash.py
@@ -15,12 +15,12 @@ def update_data_and_graph(start_time, end_time):
     cursor = conn.cursor()
 
     try:
-        # Запрос данных из таблицы Price за указанный временной интервал
-        cursor.execute('SELECT * FROM Price WHERE time BETWEEN ? AND ?', (start_time, end_time))
+        # Запрос данных из таблицы Price за указанный временной интервал HA
+        cursor.execute('SELECT * FROM PriceHark WHERE time BETWEEN ? AND ?', (start_time, end_time))
         price_data = cursor.fetchall()
 
         # Разделение данных на списки для каждого столбца
-        time_price = [row[6] for row in price_data]
+        time_price = [row[11] for row in price_data]
         trakt_salt_price = [int(row[1]) for row in price_data]
         derzhprom_nikolskii_price = [int(row[2]) for row in price_data]
         dafi_uvileynii_price = [int(row[3]) for row in price_data]
@@ -36,8 +36,100 @@ def update_data_and_graph(start_time, end_time):
         fig_price.add_trace(go.Scatter(x=time_price, y=plechan_moroz_price, mode='lines', name='Георгія Тарасенка (Плеханівська) вулиця, 43/1 - Морозова вулиця, 20А/1, 84 грн'))
         fig_price.add_trace(go.Scatter(x=time_price, y=plitk_pastera_price, mode='lines', name='Плиткова вулиця, 4 - Луї Пастера вулиця, 234А/1, 56 грн'))
         fig_price.update_layout(title='Цены OnTaxi Харьков', xaxis_title='Время', yaxis_title='Цена')
+        # ZAp
+        cursor.execute('SELECT * FROM PriceZap WHERE time BETWEEN ? AND ?', (start_time, end_time))
+        price_data_zap = cursor.fetchall()
 
-        return fig_price
+        
+        time_price_zap = [row[11] for row in price_data_zap]
+        krut_perem_price = [int(row[1]) for row in price_data_zap]
+        avram_aptech_price = [int(row[2]) for row in price_data_zap]
+        popovka_daln_price = [int(row[3]) for row in price_data_zap]
+        ruban_budiv_price = [row[4] for row in price_data_zap]
+        tenis_tepli_price = [row[5] for row in price_data_zap]
+      
+
+       
+        fig_price_zap = go.Figure()
+        fig_price_zap.add_trace(go.Scatter(x=time_price_zap, y=krut_perem_price, mode='lines', name='Героїв Крут (12 Квітня) вулиця, 3 - Перемоги вулиця, 87В / 77 грн'))
+        fig_price_zap.add_trace(go.Scatter(x=time_price_zap, y=avram_aptech_price, mode='lines', name='Авраменка вулиця, 1 - Аптечна вулиця, 3 / 75 грн'))
+        fig_price_zap.add_trace(go.Scatter(x=time_price_zap, y=popovka_daln_price, mode='lines', name='Балка Поповка вулиця, 21 - Дальня вулиця, 41/2 / 95 грн'))
+        fig_price_zap.add_trace(go.Scatter(x=time_price_zap, y=ruban_budiv_price, mode='lines', name='Рубана вулиця, 20 - Будівельників бульвар, 1 / 59 грн'))
+        fig_price_zap.add_trace(go.Scatter(x=time_price_zap, y=tenis_tepli_price, mode='lines', name='Тенісна вулиця, 20 - Теплична вулиця, 1 / 77 грн'))
+        fig_price_zap.update_layout(title='Цены OnTaxi Запорожье', xaxis_title='Время', yaxis_title='Цена')
+
+        # Dnepr
+        cursor.execute('SELECT * FROM PriceDnepr WHERE time BETWEEN ? AND ?', (start_time, end_time))
+        price_data_dnepr = cursor.fetchall()
+
+        
+        time_price_dnepr = [row[11] for row in price_data_dnepr]
+        mironova_makar_price = [int(row[1]) for row in price_data_dnepr]
+        zapor_zapor_price = [int(row[2]) for row in price_data_dnepr]
+        peremog_peremog_price = [int(row[3]) for row in price_data_dnepr]
+        novoch_pravdi_price = [row[4] for row in price_data_dnepr]
+        zoryana_zlavni_price = [row[5] for row in price_data_dnepr]
+      
+
+       
+        fig_price_dnepr = go.Figure()
+        fig_price_dnepr.add_trace(go.Scatter(x=time_price_dnepr, y=mironova_makar_price, mode='lines', name='Європейська (Миронова) вулиця, 8А - Макарова вулиця, 1А / 114 грн'))
+        fig_price_dnepr.add_trace(go.Scatter(x=time_price_dnepr, y=zapor_zapor_price, mode='lines', name='Запорізьке шосе, 72 - Запорізьке шосе, 27Б / 74 грн'))
+        fig_price_dnepr.add_trace(go.Scatter(x=time_price_dnepr, y=peremog_peremog_price, mode='lines', name='Набережна Перемоги вулиця, 134 - Набережна Перемоги вулиця, 2Г / 113 грн'))
+        fig_price_dnepr.add_trace(go.Scatter(x=time_price_dnepr, y=novoch_pravdi_price, mode='lines', name='Переволочанський (Новочеркаський) провулок, 5 - Слобожанський (Газети "Правда") проспект, 5А / 117 грн'))
+        fig_price_dnepr.add_trace(go.Scatter(x=time_price_dnepr, y=zoryana_zlavni_price, mode='lines', name='Зоряна вулиця, 4 - Славни вулиця, 1 / 66 грн'))
+        fig_price_dnepr.update_layout(title='Цены OnTaxi Днепр', xaxis_title='Время', yaxis_title='Цена')
+
+        #KR
+
+        cursor.execute('SELECT * FROM PriceKR WHERE time BETWEEN ? AND ?', (start_time, end_time))
+        price_data_kr = cursor.fetchall()
+
+        
+        time_price_kr = [row[11] for row in price_data_kr]
+        franc_avto_price = [int(row[1]) for row in price_data_kr]
+        indus_sonyach_price = [int(row[2]) for row in price_data_kr]
+        prosk_goverl_price = [int(row[3]) for row in price_data_kr]
+        turn_grud_price = [row[4] for row in price_data_kr]
+        glazunov_centr_price = [row[5] for row in price_data_kr]
+      
+
+       
+        fig_price_kr = go.Figure()
+        fig_price_kr.add_trace(go.Scatter(x=time_price_kr, y=franc_avto_price, mode='lines', name='Французька (Комінтернівська) вулиця, 7 -> Автомеханічна (Кисловодська) вулиця, 1 (60)'))
+        fig_price_kr.add_trace(go.Scatter(x=time_price_kr, y=indus_sonyach_price, mode='lines', name='Індустріальний мікрорайон, 68 -> Сонячний мікрорайон, 3 (64)'))
+        fig_price_kr.add_trace(go.Scatter(x=time_price_kr, y=prosk_goverl_price, mode='lines', name='Проскурівська вулиця, 31А -> Говерлівська (Самойлова) вулиця, 82 (79)'))
+        fig_price_kr.add_trace(go.Scatter(x=time_price_kr, y=turn_grud_price, mode='lines', name='Турнірна вулиця, 10 -> Груднева вулиця, 1 (55)'))
+        fig_price_kr.add_trace(go.Scatter(x=time_price_kr, y=glazunov_centr_price, mode='lines', name='Старотернівська (Глазунова) вулиця, 106 -> Центральна вулиця, 1А (79)'))
+        fig_price_kr.update_layout(title='Цены OnTaxi Кривой Рог', xaxis_title='Время', yaxis_title='Цена')
+    
+        # Zt
+
+        cursor.execute('SELECT * FROM PriceZhitom WHERE time BETWEEN ? AND ?', (start_time, end_time))
+        price_data_zt = cursor.fetchall()
+
+        
+        time_price_zt = [row[11] for row in price_data_zt]
+        mazep_epic_price = [int(row[1]) for row in price_data_zt]
+        novog_hinch_price = [int(row[2]) for row in price_data_zt]
+        korol_komun_price = [int(row[3]) for row in price_data_zt]
+        shors_novaposh_price = [row[4] for row in price_data_zt]
+        knyas_koval_price = [row[5] for row in price_data_zt]
+      
+
+       
+        fig_price_zt = go.Figure()
+        fig_price_zt.add_trace(go.Scatter(x=time_price_zt, y=mazep_epic_price, mode='lines', name='І. Мазепи (Мануїльського) вулиця, 164 -> Пункт Незламності (Епіцентр) (59)'))
+        fig_price_zt.add_trace(go.Scatter(x=time_price_zt, y=novog_hinch_price, mode='lines', name='Новогоголівська вулиця, 42/1 -> Хінчанський проїзд, 1/67 (70)'))
+        fig_price_zt.add_trace(go.Scatter(x=time_price_zt, y=korol_komun_price, mode='lines', name='Корольова вулиця, 101 -> Комунальний провулок, 1 (58)'))
+        fig_price_zt.add_trace(go.Scatter(x=time_price_zt, y=shors_novaposh_price, mode='lines', name='Покровська (Щорса) вулиця, 142 -> Пункт Незламності (Нова пошта №27) (72)'))
+        fig_price_zt.add_trace(go.Scatter(x=time_price_zt, y=knyas_koval_price, mode='lines', name='Князькова вулиця, 39А -> Є. Коновальця (Якубовського) вулиця, 7А (90)'))
+        fig_price_zt.update_layout(title='Цены OnTaxi Житомир', xaxis_title='Время', yaxis_title='Цена')
+        
+
+        return fig_price, fig_price_zap, fig_price_dnepr, fig_price_kr, fig_price_zt
+
+
     except Exception as e:
         print("Ошибка при обновлении графика:", e)
         return None
@@ -47,7 +139,11 @@ def update_data_and_graph(start_time, end_time):
 
 # Определение макета веб-приложения
 app.layout = html.Div(children=[
-    dcc.Graph(id='price-graph'),
+    dcc.Graph(id='price-graph-hark'),
+    dcc.Graph(id='price-graph-zap'),
+    dcc.Graph(id='price-graph-dnepr'),
+    dcc.Graph(id='price-graph-kr'),
+    dcc.Graph(id='price-graph-zt'),
     dcc.DatePickerRange(
         id='date-picker-range',
         start_date=datetime.datetime.now() - datetime.timedelta(days=1),
@@ -63,7 +159,7 @@ app.layout = html.Div(children=[
 ])
 
 # Определение функции обновления графика по интервалу и при включении чек-бокса
-@app.callback(Output('price-graph', 'figure'),
+@app.callback(Output('price-graph-hark', 'figure'),Output('price-graph-zap','figure'),Output('price-graph-dnepr','figure'),Output('price-graph-kr','figure'),Output('price-graph-zt','figure'),
               [Input('date-picker-range', 'start_date'),
                Input('date-picker-range', 'end_date'),
                Input('auto-update-checklist', 'value'),
